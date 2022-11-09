773c27cf0a0b264b65091a6e024cc040df11fb77
Claudio Fernandes <claudio.fe.rejes@gmail.com>
2022-11-09 00:12:45 -0300

    git commit -m "Projeto2-Docker Swarm com Vagrant"

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com | sudo bash
+sudo curl -fsSL "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker vagrant
+sudo cd / && sudo mkdir dados 
+docker run --name meu-nginx -p 8080:80 -v /dados:/usr/share/nginx/html:ro -d nginx
